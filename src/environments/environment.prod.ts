d06a235b4f3d143bb203b703233be0f2b6ae0d60
Johan Ahlstrand <042ahlstrand@gmail.com>
2019-05-10 21:38:27 +0200

    public use, removed credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAuV02VgK47hXYgOTM6X9uYyrDsrRYhmGM",
-    authDomain: "ng-blog-cd79c.firebaseapp.com",
-    databaseURL: "https://ng-blog-cd79c.firebaseio.com",
-    projectId: "ng-blog-cd79c",
-    storageBucket: "ng-blog-cd79c.appspot.com",
-    messagingSenderId: "862590256859",
-    appId: "1:862590256859:web:82a386348b232094"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAuV02VgK47hXYgOTM6X9uYyrDsrRYhmGM",
-    authDomain: "ng-blog-cd79c.firebaseapp.com",
-    databaseURL: "https://ng-blog-cd79c.firebaseio.com",
-    projectId: "ng-blog-cd79c",
-    storageBucket: "ng-blog-cd79c.appspot.com",
-    messagingSenderId: "862590256859",
-    appId: "1:862590256859:web:82a386348b232094"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
 };
 
